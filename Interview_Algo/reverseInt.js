fcf27a0ddd5f5951ecc350603ad1ecac00c0fe76
punchme2 <smyth.isaiah@gmail.com>
2023-07-29 12:55:10 -0600

    commit

--- Interview_Algo/reverseInt.js
+++ Interview_Algo/reverseInt.js
@@ -1,3 +1,22 @@
 function reverseInt(n) {
-    
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse().join('')
+    if (n < 0){
+        return parseInt(reversed) * -1;
+    }
+    return parsedInt(reversed);
+
+}
+
+
+function reverseInt(n) {
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse().join('')
+
+    return parsedInt(reversed) * Math.sign(n);
+
 }
